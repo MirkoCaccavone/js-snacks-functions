217d37f23b125b4df1f1bb236dc08117c9b0d17d
Mirko <mirkocaccavone03@gmail.com>
2025-01-09 15:58:01 +0100

    [feat] add function es.bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -3,15 +3,37 @@ Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
 e buonasera se è sera (oltre le 17)
+in base all'orario aggiornato sul dispositivo dell'utente.
 */
 
 const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(nome){
+    const date = new Date();
+    const hours = date.getHours();
+    let saluto = "";
+    
+    if(hours < 13){
+        saluto = "Buongiorno";
+
+    }else if(hours < 18){
+        saluto = "Buon pomeriggio";
+
+    }else{
+        saluto = "Buonasera";
+    }
+
+    return saluto +" "+ nome;
+    // return `${saluto} ${nome}`;
+    
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(saluto(name));
 
 
 
